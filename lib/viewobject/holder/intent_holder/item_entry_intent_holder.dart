b76c7bd9691c67985db363f8f4bd362ffdf5c2ce
IbrahamAdams <mm1499853@gmail.com>
2025-01-14 17:14:24 +0300

    first commit

---
+++ lib/viewobject/holder/intent_holder/item_entry_intent_holder.dart
@@ -0,0 +1,10 @@
+import 'package:flutterbuyandsell/viewobject/product.dart';
+
+class ItemEntryIntentHolder {
+  const ItemEntryIntentHolder({
+    required this.flag,
+    required this.item,
+  });
+  final String flag;
+  final Product? item;
+}
